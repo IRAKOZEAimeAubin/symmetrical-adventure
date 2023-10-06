c4571faae16e46f2b443ecb7401ad6e81b34c120
Irakoze Aime Aubin <irakozeaimeaubin@gmail.com>
2023-10-06 18:54:55 +0200

    commit #1

--- features/iceCream/iceCreamSlice.js
+++ features/iceCream/iceCreamSlice.js
@@ -1,4 +1,5 @@
 const { createSlice } = require( '@reduxjs/toolkit' );
+const { cakeActions } = require( '../cake/cakeSlice' );
 
 const initialState = {
     numOfIceCreams: 20
@@ -14,6 +15,16 @@ const iceCreamSlice = createSlice( {
         restocked: ( state, action ) => {
             state.numOfIceCreams += action.payload;
         }
+    },
+    // extraReducers: {
+    //     [ 'cake/ordered' ]: (state) => {
+    //         state.numOfIceCreams--;
+    //     }
+    // }
+    extraReducers: ( builder ) => {
+        builder.addCase( cakeActions.ordered, ( state ) => {
+            state.numOfIceCreams--;
+        } );
     }
 } );
 
--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const store = require( "./app/store" );
 const { cakeActions } = require( "./features/cake/cakeSlice" );
 const { iceCreamActions } = require( "./features/iceCream/iceCreamSlice" );
+const fetchUsers = require( './features/user/userSlice' ).fetchUsers;
 
 const log = console.log;
 
 log( 'Initial state: ', store.getState() );
-const unsubscribe = store.subscribe( () => log( 'Updated state: ', store.getState() ) );
+const unsubscribe = store.subscribe( () => { } );
 
-store.dispatch( cakeActions.ordered( 2 ) );
-store.dispatch( cakeActions.ordered( 3 ) );
-store.dispatch( cakeActions.restocked( 7 ) );
+// store.dispatch( cakeActions.ordered( 2 ) );
+// store.dispatch( cakeActions.ordered( 3 ) );
+// store.dispatch( cakeActions.restocked( 7 ) );
 
-store.dispatch( iceCreamActions.ordered( 5 ) );
-store.dispatch( iceCreamActions.restocked( 11 ) );
-unsubscribe();
+// store.dispatch( iceCreamActions.ordered( 5 ) );
+// store.dispatch( iceCreamActions.restocked( 11 ) );
+
+store.dispatch( fetchUsers() );
+
+// unsubscribe();
