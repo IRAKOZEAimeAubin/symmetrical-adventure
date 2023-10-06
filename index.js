92fb187ed25971d60c92edbc4b07b2b1161502a6
Irakoze Aime Aubin <irakozeaimeaubin@gmail.com>
2023-10-06 18:54:55 +0200

    commit #1

---
+++ index.js
@@ -0,0 +1,16 @@
+const store = require( "./app/store" );
+const { cakeActions } = require( "./features/cake/cakeSlice" );
+const { iceCreamActions } = require( "./features/iceCream/iceCreamSlice" );
+
+const log = console.log;
+
+log( 'Initial state: ', store.getState() );
+const unsubscribe = store.subscribe( () => log( 'Updated state: ', store.getState() ) );
+
+store.dispatch( cakeActions.ordered( 2 ) );
+store.dispatch( cakeActions.ordered( 3 ) );
+store.dispatch( cakeActions.restocked( 7 ) );
+
+store.dispatch( iceCreamActions.ordered( 5 ) );
+store.dispatch( iceCreamActions.restocked( 11 ) );
+unsubscribe();
